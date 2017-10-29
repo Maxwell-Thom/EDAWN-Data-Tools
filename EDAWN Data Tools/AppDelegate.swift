240a92b2c29b077cac9d2b9e978dedd118afb76e
Maxwell-Thom <maxwell_thom@nevada.unr.edu>
2017-10-29 13:59:47 -0700

    Initial Commit

---
+++ EDAWN Data Tools/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EDAWN Data Tools
+//
+//  Created by maxwell thom on 10/29/17.
+//  Copyright © 2017 maxwell thom. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+   func applicationDidFinishLaunching(_ aNotification: Notification) {
+      // Insert code here to initialize your application
+   }
+
+   func applicationWillTerminate(_ aNotification: Notification) {
+      // Insert code here to tear down your application
+   }
+
+
+}
+
---
+++ EDAWN Data Tools/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EDAWN Data Tools
+//
+//  Created by maxwell thom on 10/29/17.
+//  Copyright © 2017 maxwell thom. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+   }
+
+   override var representedObject: Any? {
+      didSet {
+      // Update the view, if already loaded.
+      }
+   }
+
+
+}
+
