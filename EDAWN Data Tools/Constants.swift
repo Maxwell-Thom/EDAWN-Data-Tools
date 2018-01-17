afbb55a5069c36db012d3cd610ca47290a41bcb5
Maxwell-Thom <maxwell_thom@nevada.unr.edu>
2018-01-17 15:02:07 -0800

    rebuilt for caspio

---
+++ EDAWN Data Tools/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  EDAWN Data Tools
+//
+//  Created by maxwell thom on 1/11/18.
+//  Copyright © 2018 maxwell thom. All rights reserved.
+//
+
+import Foundation
+
+public struct Constants {
+   static let caspioClientID = "c0c311c790a2416569dd581bb15c65621357d41118ed3e2deb"
+   static let caspioClientSecret = "db740a06f62e4e048011a448933312ccab03a49ad83f379edb"
+   static let caspioGrantType = "client_credentials"
+   static let caspioPageSize = 50
+   static let caspioAuthenticationBody = "grant_type=\(Constants.caspioGrantType)&client_id=\(Constants.caspioClientID)&client_secret=\(Constants.caspioClientSecret)"
+   static let prospectSecretKey = "5d_yyMqoJNVTy-Xx2LYG"
+}
--- EDAWN Data Tools/ViewController.swift
+++ EDAWN Data Tools/ViewController.swift
@@ -12,17 +12,23 @@ import SwiftyJSON
 import Alamofire
 
 class ViewController: NSViewController {
-   @IBOutlet weak var inputFileNameField: NSTextField!
-   @IBOutlet weak var outputFileNameField: NSTextField!
 
-   var titles = ["coo", "ceo", "founder", "cfo"]
+   var caspioAccessToken: String?
+   var caspioPage = 1
 
-   override func viewDidLoad() {
-      super.viewDidLoad()
+   @IBOutlet weak var progressBar: NSProgressIndicator!
+   @IBOutlet weak var runButton: NSButton!
+   @IBOutlet weak var inputView: NSTextField!
+   @IBOutlet weak var outputTable: NSTextField!
+   @IBOutlet weak var errorLabel: NSTextField!
+   @IBOutlet weak var inputValidationColor: NSColorWell!
+   @IBOutlet weak var outputValidationColor: NSColorWell!
 
-      // Do any additional setup after loading the view.
-
-
+   @IBAction func runRequest(_ sender: Any) {
+      self.runButton.isEnabled = false
+      errorLabel.isHidden = true
+      errorLabel.stringValue = ""
+      authenticateCaspio()
    }
 
    override var representedObject: Any? {
@@ -31,125 +37,110 @@ class ViewController: NSViewController {
       }
    }
 
-   @IBAction func inputBrowse(_ sender: Any) {
-      browseFile(textField: inputFileNameField, isOutput: false)
+   override func viewDidLoad() {
+      super.viewDidLoad()
    }
 
-   @IBAction func outputBrowse(_ sender: Any) {
-      browseFile(textField: outputFileNameField, isOutput: true)
-   }
-
-   @IBAction func submit(_ sender: Any) {
-
-      if inputFileNameField.stringValue == "" {
-         print("input path unspecified")
-         return
-      }
-
-      if outputFileNameField.stringValue == "" {
-         print("output path unspecified")
-         return
-      }
-
-      let writePath = outputFileNameField.stringValue + "/test" + ".csv"
-
-      guard let inputStream = InputStream(fileAtPath: inputFileNameField.stringValue) else {
-         print("input file path was bad")
-         return
-      }
-
-      guard let inputCSV = try? CSVReader(stream: inputStream, hasHeaderRow: true) else {
-         print("could not intialize csv reader")
-         return
-      }
-
-      guard let outputStream = OutputStream(toFileAtPath: writePath, append: false) else {
-         print("output folder path was bad")
-         return
-      }
-
-      guard let outputCSV = try? CSVWriter(stream: outputStream) else {
-         print("could not intialize csv writer")
-         return
-      }
-
-      guard let headerRows = inputCSV.headerRow else {
-         print("CSV has an invalid header")
-         return
-      }
-
-      while inputCSV.next() != nil {
-         try? outputCSV.write(row: inputCSV.currentRow!)
-      }
-      outputCSV.stream.close()
-      requestOrganization(domain: "facebook.com")
-   }
-
-   func browseFile(textField: NSTextField, isOutput: Bool) {
-      let dialog = NSOpenPanel()
-      dialog.title                   = "Choose a .csv file"
-      dialog.showsResizeIndicator    = true
-      dialog.showsHiddenFiles        = false
-      dialog.canChooseDirectories    = isOutput
-      dialog.canChooseFiles          = !isOutput
-      dialog.canCreateDirectories    = isOutput
-      dialog.allowsMultipleSelection = false
-      dialog.allowedFileTypes        = ["csv"]
-
-      if (dialog.runModal() == NSApplication.ModalResponse.OK) {
-         let result = dialog.url // Pathname of the file
-
-         if let result = result {
-            let path = result.path
-            textField.stringValue = path
-         }
-      } else {
-         // User clicked on "Cancel"
-         return
-      }
-   }
-
-   func requestOrganization(domain: String) {
-      Alamofire.request("https://api.crunchbase.com/v3.1/odm-organizations?domain_name=\(domain)&user_key=a03227a012cd7b8a686f58745bd98a0d").responseJSON { response in
-         switch response.result {
-         case .success:
-            if let data = response.data {
-               let json = JSON(data: data)
-               if let name = json["data"]["items"][0]["properties"]["name"].string {
-                  self.requestPeople(companyName: name)
+   func authenticateCaspio() {
+      Alamofire.request( "https://c5ebl095.caspio.com/oauth/token", method: .post, parameters: [:], encoding: Constants.caspioAuthenticationBody, headers: [:]).responseJSON { response in
+            switch response.result {
+            case .success:
+               if let data = response.data {
+                  let json = JSON(data: data)
+                  self.caspioAccessToken = "\(json["token_type"]) \(json["access_token"])"
+                  self.fetchCaspioProspects()
                }
+            case .failure(let error):
+               self.runButton.isEnabled = true
+               print(error)
             }
-         case .failure(let error):
-            print(error)
          }
-      }
    }
 
-   func requestPeople(companyName: String) {
-      var chiefs = [String:String]()
-      Alamofire.request("https://api.crunchbase.com/v3.1/odm-people?query=\(companyName)&user_key=a03227a012cd7b8a686f58745bd98a0d").responseJSON { response in
-         switch response.result {
-         case .success:
-            if let data = response.data {
-               let json = JSON(data: data)
-               for item in json["data"]["items"] {
-                  for title in self.titles {
-                     if item.1["properties"]["title"].string?.lowercased().range(of:title) != nil {
-                        if item.1["properties"]["organization_name"].string?.description == companyName.description {
-                           if let first = item.1["properties"]["first_name"].string, let last = item.1["properties"]["last_name"].string {
-                              chiefs["\(first) \(last)"] = "\(item.1["properties"]["title"])"
-                           }
-                        }
+   func fetchCaspioProspects(){
+      if let caspioAccessToken = caspioAccessToken {
+         let parameters: Parameters = ["q" : "{pageNumber:\(caspioPage), pageSize:\(Constants.caspioPageSize)}"]
+         Alamofire.request("https://c5ebl095.caspio.com/rest/v1/views/\(inputView.stringValue)/rows", parameters: parameters, headers: ["Authorization": caspioAccessToken]).responseJSON { response in
+            switch response.result {
+            case .success:
+               if let data = response.data {
+                  let json = JSON(data: data)
+                  print(json["Result"].count)
+                  if json["Result"].count > 0 {
+                     if json["Result"].count >= Constants.caspioPageSize {
+                        self.caspioPage = self.caspioPage+1
+                        self.runButton.title = "Run Next Page"
+                     } else {
+                        self.runButton.title = "Complete"
                      }
+
+                     for i in 0...json["Result"].count-1 {
+                        self.fetchProspectEmail(domain: json["Result"][i]["Crunchbase_Bombora_Sum_Domain"].stringValue, firstName: json["Result"][i]["people_first_name"].stringValue, lastName: json["Result"][i]["people_last_name"].stringValue, peopleUUID: json["Result"][i]["people_uuid"].stringValue)
+                     }
+                  } else {
+                     self.runButton.isEnabled = true
+                     self.runButton.title = "Run"
+                     self.errorLabel.isHidden = false
+                     self.errorLabel.stringValue = "Something went wrong when trying to get Input View!"
                   }
+
+                  self.progressBar.maxValue = Double(json["Result"].count)
+                  self.progressBar.controlSize = NSControl.ControlSize(rawValue: UInt(json["Result"].count))!
+                  self.progressBar.doubleValue = 0
                }
+            case .failure(let error):
+               self.runButton.isEnabled = true
+               print(error)
+            }
+         }
+      }
+   }
+
+   func postCaspioProspects(firstName: String, lastName: String, domainKey: String, email: String, peopleUUID: String) {
+      let json = ["People_UUID":"\(peopleUUID)",
+      "DomainKey":"\(domainKey)",
+      "First_name":"\(firstName)",
+      "Last_name":"\(lastName)",
+      "Email":"\(email)"]
+
+      if let caspioAccessToken = caspioAccessToken{
+          Alamofire.request("https://c5ebl095.caspio.com/rest/v1/tables/\(outputTable.stringValue)/rows", method: .post, parameters: json, encoding: JSONEncoding.default, headers: ["Authorization": caspioAccessToken]).response { response in
+
+            self.progressBar.increment(by: 1.0)
+            if response.response?.statusCode != 201 {
+               self.errorLabel.isHidden = false
+               self.errorLabel.stringValue = "Posting results failed with error code: \(String(response.response!.statusCode)), check Output Table Name"
+            }
+
+            if self.progressBar.doubleValue >= Double(Constants.caspioPageSize) && self.runButton.title != "Complete" {
+               self.runButton.isEnabled = true
+            }
+         }
+      }
+   }
+
+   func fetchProspectEmail(domain: String, firstName: String, lastName: String, peopleUUID: String){
+      let parameters: Parameters = ["domain" : domain, "first_name" :firstName, "last_name" : lastName]
+      Alamofire.request("https://prospect.io/api/public/v1/emails/search?", parameters: parameters, headers: ["Authorization": Constants.prospectSecretKey]).responseJSON { response in
+         switch response.result {
+         case .success:
+            if let data = response.data {
+               let json = JSON(data: data)
+               let email = json["data"][0]["attributes"]["value"].stringValue
+               self.postCaspioProspects(firstName: firstName, lastName: lastName, domainKey: domain, email: email, peopleUUID: peopleUUID)
             }
          case .failure(let error):
-            //return "\(domain): \(error.localizedDescription)"
             print(error)
          }
-         print(chiefs)
       }
+   }
+}
+
+extension String: ParameterEncoding {
+   public func encode(_ urlRequest: URLRequestConvertible, with parameters: Parameters?) throws -> URLRequest {
+      var request = try urlRequest.asURLRequest()
+      request.httpBody = data(using: .utf8, allowLossyConversion: false)
+      return request
    }
 }
 
